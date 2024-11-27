b486db47e4905acfa0d4adf32735b03e73477431
utshasaha16 <utsharoy12345@gmail.com>
2024-11-28 03:20:44 +0600

    some tea information added in server

---
+++ index.js
@@ -0,0 +1,54 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwlha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const teaCollection = client.db("teaDB").collection("tea");
+
+    app.post('/tea', async(req, res) => {
+        const newTea = req.body;
+        console.log(newTea);
+        const result = await teaCollection.insertOne(newTea);
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('tea making server is running')
+})
+
+app.listen(port, () => {
+    console.log(`tea server is running on PORT: ${port}`);
+})
